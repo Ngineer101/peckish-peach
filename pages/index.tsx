b4a595ac4fc7260d0543327bc72ea1088d444e3b
Ngineer101 <nicolaasb@pm.me>
2021-08-02 21:07:52 +0200

    NextJS, Tailwind and Supabase setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>Peckish Peach</title>
+      </Head>
+      <main></main>
+      <footer ></footer>
+    </div>
+  )
+}
