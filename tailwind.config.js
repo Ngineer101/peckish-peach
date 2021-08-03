a184626922f04744ccbf6feec59b07239dcfb7fc
Ngineer101 <nicolaasb@pm.me>
2021-08-03 19:08:00 +0200

    asset: add peach assets and configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      orange: {
+        DEFAULT: '#ffb383',
+      },
+      lightGreen: {
+        DEFAULT: '#C6E5B3',
+      },
+    },
     extend: {},
   },
   variants: {
