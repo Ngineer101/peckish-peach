2f3f890fd26940e284da2de60e903b7acc166c5e
Ngineer101 <nicolaasb@pm.me>
2021-08-03 21:18:15 +0200

    scaffold: home screen and welcome component

---
+++ components/welcome.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function Welcome() {
+  return (
+    <div className='flex items-center justify-center flex-col p-4 w-full'>
+      <div className='speech-bubble sm:w-full md:w-full lg:w-2/3 xl:w-2/3 2xl:w-2/3'>
+        <h1 className='text-center text-2xl my-4'>
+          Hello! I'm <strong>Peckish Peach</strong>, your friendly AI kitchen assistant.{' '}
+          I'm here to help you with cooking...
+        </h1>
+        <Link href='/magic-link'>
+          <a className='speech-bubble-button'>
+            Click here to continue
+          </a>
+        </Link>
+      </div>
+      <img src='/assets/peach-green.gif' height={300} width={300} />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
-import Head from 'next/head'
+import { Session } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+import { supabaseClient } from '../utils/supabaseClient';
+import axios from 'axios';
+import Welcome from '../components/welcome';
+import RecipeGenerator from '../components/recipe-generator';
+import Footer from '../components/footer';
 
 export default function Home() {
+  const [session, setSession] = useState<Session | null>(null)
+  useEffect(() => {
+    setSession(supabaseClient.auth.session())
+    supabaseClient.auth.onAuthStateChange((event, session) => {
+      setSession(session)
+      axios.post('/api/auth', { event, session }, {
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+      });
+    })
+  }, []);
+
   return (
-    <div className='container'>
-      <Head>
-        <title>Peckish Peach</title>
-      </Head>
-      <main></main>
-      <footer ></footer>
-    </div>
+    <>
+      <div className="container mt-12">
+        {
+          !session ?
+            <Welcome />
+            :
+            <RecipeGenerator />
+        }
+      </div>
+      <Footer />
+    </>
   )
 }
