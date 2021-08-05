685b7b608a8e8b79f0f6ef5187a499003f51aecc
Ngineer101 <nicolaasb@pm.me>
2021-08-05 22:44:06 +0200

    scaffold: recipe page

--- components/recipe-generator.tsx
+++ components/recipe-generator.tsx
@@ -11,6 +11,7 @@ export default function RecipeGenerator(props: {
 }) {
   const [ingredientText, setIngredientText] = useState<string>('');
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
   const fetcher = (url: string) => fetch(url, { headers: { 'token': `${props.session.access_token}` } }).then(res => res.json());
   const { data, error } = useSWR('/api/allowed-recipe-count', fetcher);
@@ -25,25 +26,36 @@ export default function RecipeGenerator(props: {
       <div className='flex items-center justify-center flex-col p-4 w-full mt-4'>
         <div className='speech-bubble sm:w-full md:w-full lg:w-2/3 xl:w-2/3 2xl:w-2/3'>
           <h1 className='text-center text-2xl my-4'>
-            Welcome! Enter ingredients you have at home and I will generate a delicious recipe for you <strong>🥗</strong>
+            Welcome! Enter <strong>at least 4 ingredients</strong> you have at home and I will generate a delicious recipe for you <strong>🥗</strong>
           </h1>
           <textarea className='border border-solid border-black rounded-xl outline-none p-4 w-4/5' style={{ minHeight: 120 }}
             value={ingredientText} onChange={(evt) => setIngredientText(evt.target.value)}
             placeholder={'Cheese\nPasta\nTomatoes'}></textarea>
+          {
+            errorMessage &&
+            <label className='text-red-500 text-center font-bold my-2'>{errorMessage}</label>
+          }
           <button className='speech-bubble-button'
+            disabled={data && data.allowedRecipeCount === 0}
             onClick={() => {
               if (ingredientText) {
-                setLoading(true);
                 const ingredients = ingredientText.split('\n');
+                if (ingredients.length < 4) {
+                  setErrorMessage('Please enter at least 4 ingredients.');
+                  return;
+                }
+
+                setLoading(true);
                 axios.post('/api/generate-recipe', { ingredients },
                   { headers: { 'token': `${props.session.access_token}` } })
                   .then(response => {
                     setLoading(false);
+                    setErrorMessage('');
                     router.push(`/recipe/${response.data.id}`);
                   })
                   .catch(error => {
                     setLoading(false);
-                    // TODO: Show nice error message
+                    setErrorMessage('An error occurred while generating a recipe. Please refresh the page and try again.');
                   });
               }
             }}>
---
+++ pages/recipe/[id].tsx
@@ -0,0 +1,83 @@
+import { GetServerSideProps, GetServerSidePropsResult } from 'next';
+import Link from 'next/link';
+import Footer from '../../components/footer';
+import SignOutButton from '../../components/signoutButton';
+import { supabaseClient } from '../../utils/supabaseClient';
+
+export default function Recipe(props: {
+  recipe: {
+    title: string,
+    instructions: string[],
+    ingredients: string[],
+  },
+}) {
+  return (
+    <>
+      <div className='container'>
+        <header>
+          <Link href='/'>
+            <a className='default-button mr-4'>Home</a>
+          </Link>
+          <Link href='/my-recipes'>
+            <a className='default-button mr-4'>My recipes</a>
+          </Link>
+          <SignOutButton />
+        </header>
+        <h1 className='text-center text-5xl font-bold my-8 underline'
+          style={{ transform: "rotate(-1deg)" }}>{props.recipe.title.replace(':', '')}</h1>
+        <div className='flex flex-col items-center'>
+          <div className='flex flex-col items-center bg-gray-200 rounded-xl shadow-2xl my-8 p-4 w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2'
+            style={{ transform: 'rotate(1deg)' }}>
+            <h3 className='text-center text-3xl mb-4 underline'>Ingredients</h3>
+            {
+              props.recipe && props.recipe.ingredients && props.recipe.ingredients.length > 0 &&
+              props.recipe.ingredients.map((ingredient, i) =>
+                <div key={i}>
+                  <span className='text-lg my-1 text-center'>{ingredient}</span>
+                </div>
+              )
+            }
+          </div>
+          <div className='flex flex-col items-center bg-gray-200 rounded-xl shadow-2xl my-8 p-4 w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2'
+            style={{ transform: 'rotate(-1deg)' }}>
+            <h3 className='text-center text-3xl mb-4 underline'>Instructions</h3>
+            {
+              props.recipe && props.recipe.instructions && props.recipe.instructions.length > 0 &&
+              props.recipe.instructions.map((instruction, i) =>
+                <div key={i}>
+                  <span className='text-lg my-1 text-center'>{instruction}</span>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: any): Promise<GetServerSidePropsResult<{}>> => {
+  supabaseClient.auth.setAuth(context.req.cookies["sb:token"]);
+  const { user } = await supabaseClient.auth.api.getUserByCookie(context.req)
+  if (user) {
+    const { data, error } = await supabaseClient
+      .from('recipes')
+      .select()
+      .eq('user_id', user!.id)
+      .eq('id', context.query.id)
+      .single();
+
+    return {
+      props: {
+        recipe: data,
+      }
+    }
+  }
+
+  return {
+    props: {
+      recipe: {},
+    }
+  }
+}
