d263c658b8747afe82d2275780a076a070a9372e
Ngineer101 <nicolaasb@pm.me>
2021-08-11 20:57:56 +0200

    feature: add delete logic for grocery list

--- pages/my-grocery-lists.tsx
+++ pages/my-grocery-lists.tsx
@@ -8,11 +8,17 @@ import { supabaseClient } from '../utils/supabaseClient';
 import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
 import PlusIcon from '@heroicons/react/outline/PlusIcon';
+import axios from 'axios';
+import { useState } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default function MyGroceryLists(props: {
   groceryLists: IGroceryList[],
 }) {
   const router = useRouter();
+  const session = supabaseClient.auth.session();
+  const [groceryLists, setGroceryLists] = useState(props.groceryLists || []);
   return (
     <>
       <Head>
@@ -31,20 +37,48 @@ export default function MyGroceryLists(props: {
         </div>
         <div className='flex flex-col items-center'>
           {
-            props.groceryLists && props.groceryLists.length > 0 &&
-            props.groceryLists.map((groceryList, i) =>
+            groceryLists && groceryLists.length > 0 &&
+            groceryLists.map((groceryList, i) =>
               <GroceryListCard
                 key={i}
                 name={groceryList.name}
                 href={`/grocery-list/${groceryList.id}`}
                 onEditClick={(evt) => router.push(`/grocery-list/${groceryList.id}/edit`)}
                 onDeleteClick={(evt) => {
-                  // TODO: Add delete logic
+                  confirmAlert({
+                    customUI: ({ onClose }) => {
+                      return (
+                        <div className='custom-ui'>
+                          <h1 className='text-center my-4'>Are you sure you want to delete this grocery list?</h1>
+                          <div className='flex justify-between px-4'>
+                            <button className='flex-1 py-2 border border-red-500 border-solid text-red-500 rounded-lg mr-2'
+                              onClick={onClose}>No</button>
+                            <button className='flex-1 py-2 rounded-lg border border-solid border-black'
+                              onClick={() => {
+                                axios.delete(`/api/save-grocery-list?id=${groceryList.id}`, {
+                                  headers: {
+                                    'token': `${session?.access_token}`
+                                  }
+                                })
+                                  .then(() => {
+                                    groceryLists.splice(i, 1);
+                                    setGroceryLists([...groceryLists]);
+                                  })
+                                  .catch(error => {
+                                    // TODO: Handle error
+                                  });
+                                onClose();
+                              }}>Yes, delete it!</button>
+                          </div>
+                        </div>
+                      );
+                    }
+                  });
                 }} />
             )
           }
           {
-            (!props.groceryLists || props.groceryLists.length == 0) &&
+            (!groceryLists || groceryLists.length == 0) &&
             <span className='text-center text-black font-bold text-lg p-4 rounded-xl shadow-lg bg-orange my-4 mx-2'>
               You don't have any grocery lists yet.
             </span>
